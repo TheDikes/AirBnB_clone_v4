a2959fd9dbdfedf751b6d952cbc8cc0158fc3b23
Frankenstei <enwereloveday@yahoo.com>
2023-05-28 12:36:45 -0700

    all

--- api/v1/app.py
+++ api/v1/app.py
@@ -17,6 +17,11 @@ def downtear(self):
     '''Status of your API'''
     storage.close()
 
+@app.errorhandler(404)
+def page_not_found(error):
+    '''return render_template'''
+    return jsonify(('error'='Not found'), 404)    
+
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST')
     port = getenv('HBNB_API_PORT')
