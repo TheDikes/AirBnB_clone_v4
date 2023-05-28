e5823cd97026d6eaf5d11713b64ca04ff671deda
Frankenstei <enwereloveday@yahoo.com>
2023-05-28 14:28:05 -0700

    all

---
+++ api/v1/app1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Flask App
+"""
+from flask import Flask, jsonify, abort
+from os import getenv
+from api.v1.views import app_views
+from models import storage
+
+# Global Flask Application Variable: app
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def downtear(self):
+    '''Status of your API'''
+    storage.close()
+
+
+@app.errorhandler(404)
+def invalid_route(e):
+    return jsonify({'errorCode': 404, 'error': 'Not found'})
+
+if __name__ == "__main__":
+    host = getenv('HBNB_API_HOST')
+    port = getenv('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, threaded=True)
